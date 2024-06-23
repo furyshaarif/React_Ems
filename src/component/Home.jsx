a6471c09044003afeb27f35801c3faa01a6c1f0f
Mohammad Shaarif <110607876+furyshaarif@users.noreply.github.com>
2024-06-23 21:49:44 +0530

    Add files via upload

---
+++ src/component/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div><h1>
+        Employee menu
+        </h1>
+        <button>
+            Add New Employee Data
+        </button>
+        </div>
+  )
+}
+
+export default Home
